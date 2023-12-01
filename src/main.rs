7e1e66a5e7cd6458832b46f54e0da5c1c853c29e
lancekmyers <lancekmyers@gmail.com>
2023-12-01 03:01:23 -0600

    max, min, first, last

--- src/main.rs
+++ src/main.rs
@@ -65,9 +65,119 @@ impl<A: std::ops::Add<Output = A> + From<u32>> Fold for Sum<A> {
     }
 }
 
+struct Max<A> {
+    ghost: PhantomData<A>,
+}
+
+impl<A: std::cmp::Ord> Fold1 for Max<A> {
+    type A = A;
+
+    type B = A;
+
+    type M = A;
+
+    fn new() -> Self {
+        Max { ghost: PhantomData }
+    }
+
+    fn init(self: &Self, x: Self::A) -> Self::M {
+        x
+    }
+
+    fn step(self: &Self, x: Self::A, acc: Self::M) -> Self::M {
+        std::cmp::max(x, acc)
+    }
+
+    fn output(self: &Self, acc: Self::M) -> Self::B {
+        acc
+    }
+}
+
+struct Min<A> {
+    ghost: PhantomData<A>,
+}
+
+impl<A: std::cmp::Ord> Fold1 for Min<A> {
+    type A = A;
+
+    type B = A;
+
+    type M = A;
+
+    fn new() -> Self {
+        Min { ghost: PhantomData }
+    }
+
+    fn init(self: &Self, x: Self::A) -> Self::M {
+        x
+    }
+
+    fn step(self: &Self, x: Self::A, acc: Self::M) -> Self::M {
+        std::cmp::min(x, acc)
+    }
+
+    fn output(self: &Self, acc: Self::M) -> Self::B {
+        acc
+    }
+}
+
+struct First<A> {
+    ghost: PhantomData<A>,
+}
+
+impl<A> Fold1 for First<A> {
+    type A = A;
+    type B = A;
+    type M = A;
+
+    fn new() -> Self {
+        First { ghost: PhantomData }
+    }
+
+    fn init(self: &Self, x: Self::A) -> Self::M {
+        x
+    }
+
+    fn step(self: &Self, x: Self::A, acc: Self::M) -> Self::M {
+        acc
+    }
+
+    fn output(self: &Self, acc: Self::M) -> Self::B {
+        acc
+    }
+}
+
+struct Last<A> {
+    ghost: PhantomData<A>,
+}
+
+impl<A> Fold1 for Last<A> {
+    type A = A;
+    type B = A;
+    type M = A;
+
+    fn new() -> Self {
+        Last { ghost: PhantomData }
+    }
+
+    fn init(self: &Self, x: Self::A) -> Self::M {
+        x
+    }
+
+    fn step(self: &Self, x: Self::A, acc: Self::M) -> Self::M {
+        x
+    }
+
+    fn output(self: &Self, acc: Self::M) -> Self::B {
+        acc
+    }
+}
+
 fn main() {
-    let xs: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    let xs: Vec<i64> = vec![1, 2, 3, 4, 5];
     let summer = Sum::new();
-    let total = run_fold(summer, xs.into_iter());
+    let total = run_fold(summer, xs.clone().into_iter());
+    let max = run_fold1(Max::new(), xs.into_iter());
     println!("Sum : {total}");
+    println!("Max : {}", max.unwrap());
 }
