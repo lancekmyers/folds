8b701d3695a000fc5036d53e7075d8cf67f5d2c7
lancekmyers <lancekmyers@gmail.com>
2023-12-15 13:05:00 -0600

    simple benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,24 +1,48 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use folds::{
     self,
     common::*,
-    fold::{run_fold, run_par_fold},
+    fold::{run_fold, run_fold1, run_par_fold, Fold1},
 };
-use rayon::iter::ParallelBridge;
+use rayon::iter::IntoParallelIterator;
 
 pub fn sum_bench(c: &mut Criterion) {
     // let summer = Sum::SUM;
-    c.bench_function("fold-sum", |b: _| b.iter(|| run_fold(Sum::SUM, 0..1024)));
-    c.bench_function("par-fold-sum", |b: _| {
-        b.iter(|| run_par_fold((0..1024).par_bridge(), Sum::SUM))
+    let n = 2048;
+
+    let xs: Vec<i32> = (0..n).collect();
+    c.bench_function("fold-sum", |b: _| {
+        b.iter(|| run_fold(Sum::SUM, black_box(xs.clone().into_iter())))
     });
-    c.bench_function("builtin-sum", move |b| {
-        b.iter(move || (0..1024).sum::<i32>())
+    c.bench_function("par-fold-sum", |b: _| {
+        b.iter(|| run_par_fold(black_box(xs.clone().into_par_iter()), Sum::SUM))
     });
     c.bench_function("vec-sum", move |b| {
-        b.iter(move || (0..1024).collect::<Vec<i32>>().iter().sum::<i32>())
+        b.iter(|| black_box(xs.clone().iter()).sum::<i32>())
+    });
+    c.bench_function("builtin-sum", move |b| {
+        b.iter(|| black_box(0..n).sum::<i32>())
     });
 }
 
-criterion_group!(benches, sum_bench);
+pub fn minmax_bench(c: &mut Criterion) {
+    // let summer = Sum::SUM;
+    let n = 2048;
+    let xs: Vec<i32> = (0..n).collect();
+    c.bench_function("fold-minmax", |b: _| {
+        b.iter(|| run_fold1(Min::MIN.par(Max::MAX), black_box(0..n)))
+    });
+    // c.bench_function("par-fold-minmax", |b: _| {
+    //     b.iter(|| run_par_fold(black_box((xs.clone()[..]).par_chunks(2500)), min_max))
+    // });
+    c.bench_function("vec-minmax", move |b| {
+        b.iter(|| (xs.iter().min(), xs.iter().max()))
+    });
+    c.bench_function("builtin-minmax", move |b| {
+        b.iter(|| (black_box(0..n).min(), black_box(0..n).max()))
+    });
+}
+
+criterion_group!(benches, sum_bench, minmax_bench);
+
 criterion_main!(benches);
