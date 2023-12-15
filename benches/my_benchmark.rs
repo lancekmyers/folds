10aa94df82fed61c21561a422f8435de14712f9b
lancekmyers <lancekmyers@gmail.com>
2023-12-15 16:04:31 -0600

    benchmark min

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,7 +1,11 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use folds::{self, common::*, fold::run_fold};
+use folds::{
+    self,
+    common::*,
+    fold::{run_fold, run_fold1, Fold1},
+};
 
-fn bench(c: &mut Criterion) {
+fn bench_sum(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sum");
 
     for n in [512, 2048, 8192, 20000] {
@@ -18,6 +22,39 @@ fn bench(c: &mut Criterion) {
     group.finish();
 }
 
-// criterion_group!(benches, sum_bench, minmax_bench);
-criterion_group!(benches, bench);
+fn bench_minmax(c: &mut Criterion) {
+    let mut group = c.benchmark_group("MinMax");
+
+    for n in [512, 2048, 8192, 20000] {
+        let xs = (0..n).collect::<Vec<i32>>().into_iter();
+
+        group.bench_with_input(BenchmarkId::new("Iterator", n), &xs, |b, xs| {
+            b.iter(move || (xs.clone().min(), xs.clone().max()))
+        });
+
+        group.bench_with_input(BenchmarkId::new("Fold", n), &xs.clone(), |b, xs| {
+            b.iter(move || run_fold1(Min::MIN.par(Max::MAX), xs.clone()))
+        });
+    }
+    group.finish();
+}
+
+fn bench_min(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Min");
+
+    for n in [512, 2048, 8192, 20000] {
+        let xs = (0..n).collect::<Vec<i32>>().into_iter();
+
+        group.bench_with_input(BenchmarkId::new("Iterator", n), &xs, |b, xs| {
+            b.iter(move || xs.clone().min())
+        });
+
+        group.bench_with_input(BenchmarkId::new("Fold", n), &xs.clone(), |b, xs| {
+            b.iter(move || run_fold1(Min::MIN, xs.clone()))
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_sum, bench_minmax, bench_min);
 criterion_main!(benches);
