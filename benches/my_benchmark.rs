2f1532a2b7f169edd2070200109fff0683091db6
lancekmyers <lancekmyers@gmail.com>
2023-12-15 15:36:23 -0600

    reorganize benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use folds::{
     self,
     common::*,
@@ -34,6 +34,23 @@ pub fn minmax_bench(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, sum_bench, minmax_bench);
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Sum");
 
+    for n in [512, 2048, 8192] {
+        let xs = (0..n).collect::<Vec<i32>>().into_iter();
+
+        group.bench_with_input(BenchmarkId::new("Iterator", n), &xs, |b, xs| {
+            b.iter(move || xs.clone().sum::<i32>())
+        });
+
+        group.bench_with_input(BenchmarkId::new("Fold", n), &xs.clone(), |b, xs| {
+            b.iter(move || run_fold(Sum::SUM, xs.clone()))
+        });
+    }
+    group.finish();
+}
+
+// criterion_group!(benches, sum_bench, minmax_bench);
+criterion_group!(benches, bench);
 criterion_main!(benches);
