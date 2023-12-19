b641db8a2ea98a9db525ee9a9009e80c69a6b44f
lancekmyers <lancekmyers@gmail.com>
2023-12-18 18:10:02 -0600

    async stream parquet reader + tokio spawn (this actually hurts performance)

--- examples/taxi.rs
+++ examples/taxi.rs
@@ -1,37 +1,29 @@
 use arrow;
-use arrow::csv;
 use folds::{
     self,
     fold::{run_fold, run_fold1, run_par_fold, Fold, Fold1, FoldPar},
 };
+use parquet::arrow::async_reader;
+use parquet::arrow::{arrow_reader, ProjectionMask};
+
+use futures::{stream, StreamExt, TryStreamExt};
+
 use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
-use std::{
-    fs::File,
-    io::{BufReader, Error, Seek},
-    sync::{
-        atomic::{AtomicI32, AtomicUsize, Ordering},
-        Arc,
-    },
-};
 
-fn main() -> () {
+#[tokio::main]
+async fn main() -> () {
     let path = std::env::args().nth(1).unwrap();
-    let mut file = File::open(path).unwrap();
-    // let mut file = File::open(path).unwrap();
-    // Infer the schema with the first 100 records
-    let (schema, _) = csv::reader::Format::default()
-        .with_header(true)
-        .infer_schema(&mut file, Some(100))
-        .unwrap();
-    file.rewind().unwrap();
+    let file = tokio::fs::File::open(path).await.unwrap();
 
-    println!("{:?}", schema);
-
-    let batches = csv::reader::ReaderBuilder::new(Arc::new(schema))
-        .build(file)
+    let builder = async_reader::ParquetRecordBatchStreamBuilder::new(file)
+        .await
         .unwrap()
-        .flatten()
-        .collect::<Vec<_>>();
+        .with_batch_size(3);
+
+    let file_metadata = builder.metadata().file_metadata();
+    let mask = ProjectionMask::roots(file_metadata.schema_descr(), [3]);
+
+    let stream = builder.with_projection(mask).build().unwrap();
 
     let avg = folds::common::Sum::SUM
         .par(folds::common::Count::COUNT)
@@ -39,13 +31,37 @@ fn main() -> () {
 
     println!("Starting iteration");
 
-    batches.par_iter().for_each(|batch| {
-        let col = batch.column_by_name("passenger_count").unwrap();
-        let foo = col
-            .as_any()
-            .downcast_ref::<arrow::array::Float64Array>()
-            .unwrap();
+    let foo: Vec<(f64, u64)> = stream
+        .filter_map(|x| async { x.ok() })
+        .map(|batch| {
+            tokio::task::spawn(async move {
+                let col = batch.column(0);
+                let foo = col
+                    .as_any()
+                    .downcast_ref::<arrow::array::Float64Array>()
+                    .unwrap();
+                let mut acc = (&avg).empty();
+                (&avg).step_chunk(foo.values(), &mut acc);
+                acc
+            })
+        })
+        .buffered(4)
+        .try_collect()
+        .await
+        .unwrap();
 
-        println!("batch avg: {}", run_fold(&avg, foo.into_iter().flatten()))
+    let bar = foo.iter().fold((&avg).empty(), |mut m1, m2| {
+        (&avg).merge(&mut m1, *m2);
+        m1
     });
+    let ans = avg.output(bar);
+    // let ans: f64 = avg.output(
+    //     foo.into_iter()
+    //         .reduce(|mut m1, m2| {
+    //             avg.merge(&mut m1, m2);
+    //             m1
+    //         })
+    //         .unwrap(),
+    // );
+    println!("Average passenger_count: {ans}");
 }
