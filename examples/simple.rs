b93c29b2a4980153e0b8180878062c2003eb11d2
lancekmyers <lancekmyers@gmail.com>
2023-12-19 13:21:50 -0600

    fix up simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,11 +10,11 @@ fn main() {
 
     let fld1 = Min::MIN.par(Max::MAX);
 
-    let (s1, s2) = run_fold(fld, xs.clone().into_iter());
+    let (s1, s2) = run_fold(&fld, xs.clone().into_iter());
 
-    let (min, max) = run_fold1(fld1, xs.clone().into_iter()).unwrap();
+    let (min, max) = run_fold1(&fld1, xs.clone().into_iter()).unwrap();
 
-    let (fst, lst) = run_fold1(First::FIRST.par(Last::LAST), xs.into_iter()).unwrap();
+    let (fst, lst) = run_fold1(&First::FIRST.par(Last::LAST), xs.into_iter()).unwrap();
 
     println!("Sum : {}, {:?}", s1, s2);
     println!("Min : {}, Max {}", min, max);
@@ -28,13 +28,13 @@ fn main() {
     let avger = Count::COUNT
         .par(Sum::<f64>::SUM)
         .post_map(|(n, sum)| sum / (n as f64));
-    let avg = run_fold(avger, xs.clone().into_iter());
+    let avg = run_fold(&avger, xs.clone().into_iter());
     println!("Avg : {avg}");
 
     let avger = Count::COUNT
         .par(Sum::<f64>::SUM)
         .post_map(|(n, sum)| sum / (n as f64));
 
-    let avg = run_par_fold(xs.into_par_iter(), avger);
+    let avg = run_par_fold(xs.into_par_iter(), &avger);
     println!("Par Avg : {avg}")
 }
