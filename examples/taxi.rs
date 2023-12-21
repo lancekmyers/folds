a040ee9bfd7269588899c29be68a4f013ffd15ec
lancekmyers <lancekmyers@gmail.com>
2023-12-20 20:00:33 -0600

    use par_fold

--- examples/taxi.rs
+++ examples/taxi.rs
@@ -7,7 +7,7 @@ use parquet::arrow::ProjectionMask;
 
 use futures::StreamExt;
 
-use rayon::iter::ParallelIterator;
+use rayon::{iter::ParallelIterator, slice::ParallelSlice};
 
 #[tokio::main]
 async fn main() {
@@ -17,6 +17,8 @@ async fn main() {
 
     let threads: usize = args.next().map(|str| str.parse().unwrap()).unwrap_or(4);
     let batch_size: usize = args.next().map(|str| str.parse().unwrap()).unwrap_or(1024);
+
+    let chunk_size: usize = args.next().map(|str| str.parse().unwrap()).unwrap_or(1024);
 
     let builder = async_reader::ParquetRecordBatchStreamBuilder::new(file)
         .await
@@ -40,9 +42,20 @@ async fn main() {
                 .as_any()
                 .downcast_ref::<arrow::array::Float64Array>()
                 .unwrap();
-            let mut acc = fld.empty();
-            fld.step_chunk(col.values(), &mut acc);
-            acc
+            col.values()
+                .par_chunks(chunk_size)
+                .map(|ch| {
+                    let mut acc = fld.empty();
+                    fld.step_chunk(ch, &mut acc);
+                    acc
+                })
+                .reduce(
+                    || fld.empty(),
+                    |mut m1, m2| {
+                        fld.merge(&mut m1, m2);
+                        m1
+                    },
+                )
         })
         .buffered(threads)
         .collect()
